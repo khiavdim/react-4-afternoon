34d52ca6349ffa918cfe17d8def071fc05fd9a98
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-07-02 14:23:59 -0600

    Finished and Finished Black Diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: []
+    };
   }
 
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then(res => {
+        this.setState({
+          students: res.data
+        });
+      })
+      .catch(err => console.log("Error getting students", err));
+  }
+
+  goBack = () => {
+    this.props.history.goBack();
+  };
+
   render() {
+    let { students } = this.state;
+    let studentsArr = students.map(student => (
+      <Link to={`/student/${student.id}`} key={student.id}>
+        <h3>
+          {student.first_name} {student.last_name}
+        </h3>
+      </Link>
+    ));
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {studentsArr}
+        <p>&emsp;</p>
+        <button onClick={this.goBack}>{"< Go back"}</button>
       </div>
-    )
+    );
   }
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Student extends Component {
-  constructor() {
-    super()
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentInfo: {},
+    }
   }
 
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          studentInfo: res.data
+        });
+      })
+      .catch(err => console.log("Error getting student info", err));
+  }
+
+  goBack = () => {
+    this.props.history.goBack();
+}
+
   render() {
+    let { studentInfo } = this.state;
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>
+          {studentInfo.first_name} {studentInfo.last_name}
+        </h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
+        <p>&emsp;</p>
+        <button onClick={this.goBack}>{"< Go back"}</button>
       </div>
-    )
+    );
   }
 }
